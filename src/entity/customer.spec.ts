54533852e464fe3c249caf1ac3526079aa4cf01e
André Freitas <freitasandre38@gmail.com>
2024-06-13 09:07:37 -0300

    feat: create more tests and fix throw error

--- src/entity/customer.spec.ts
+++ src/entity/customer.spec.ts
@@ -4,6 +4,22 @@ describe("Customer unit test", () => {
     it("Should throw error when id is null", () => {
         expect(() => {
             let customer = new Customer("", "John");
-        }).toThrowError("Id is required");
+        }).toThrow("Id is required");
+    })
+
+    it("Should deactivate customer", () => {
+        expect(() => {
+            let customer = new Customer("1", "John");
+            customer.deactivate();
+            expect(customer._active).toBe(false)
+        })
+    })
+
+    it("Should activate customer", () => {
+        expect(() => {
+            let customer = new Customer("1", "John");
+            customer.activate();
+            expect(customer._active).toBe(true)
+        })
     })
 });
