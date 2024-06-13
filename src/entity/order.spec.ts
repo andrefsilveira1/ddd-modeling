10cb672b9db3e35d36885760d9b89131a9c777e2
André Freitas <freitasandre38@gmail.com>
2024-06-13 09:42:36 -0300

    chore: change aggregate product

--- src/entity/order.spec.ts
+++ src/entity/order.spec.ts
@@ -3,19 +3,19 @@ import Order from "./order";
 describe("Order unit tests", () => {
     it("Should throw error when empty", () => {
         expect(() => {
-            let order = new Order("", "aaa", [],0);
+            let order = new Order("", "aaa", [],0, "prod1", 5);
         }).toThrow("Id is required")
     });
 
     it("Should throw error when empty", () => {
         expect(() => {
-            let order = new Order("123", "", [],0);
+            let order = new Order("123", "", [],0, "prod2", 1);
         }).toThrow("Customer Id is required")
     });
 
     it("Should throw error when empty", () => {
         expect(() => {
-            let order = new Order("123", "124", [],0);
+            let order = new Order("123", "124", [],0, "prod3", 4);
         }).toThrow("Items are required")
     });
 
--- src/entity/order.ts
+++ src/entity/order.ts
@@ -2,15 +2,19 @@ import OrderItem from "./items";
 
 export default class Order {
     private _id: string;
+    private _productId: string;
     private _customerId: string;
     private _items: OrderItem[];
     private _total: number;
+    private _quantity: number;
 
-    constructor(id: string, customerId: string, items: OrderItem[], total: number) {
+    constructor(id: string, customerId: string, items: OrderItem[], total: number, productId: string, quantity: number) {
         this._id = id;
         this._customerId = customerId;
         this._items = items;
         this._total = total;
+        this._productId = productId;
+        this._quantity = quantity;
         this.validate();
     }
 
