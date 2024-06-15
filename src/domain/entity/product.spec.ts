3956f9067b142a6d87554860382c87ba1bff8d31
André Freitas <freitasandre38@gmail.com>
2024-06-15 17:47:48 -0300

    refactor: remake product unit test

--- src/domain/entity/product.spec.ts
+++ src/domain/entity/product.spec.ts
@@ -4,19 +4,31 @@ describe("Product unit tests", () => {
     it("Should throw error when id is empty", () => {
         expect(() => {
             let product = new Product("", "Product", 100);
-        }).toThrow("Id is required")
+        }).toThrowError("Id is required")
     });
 
     it("Should throw error when name is empty", () => {
         expect(() => {
             let product = new Product("1", "", 100);
-        }).toThrow("Name is required")
+        }).toThrowError("Name is required")
     });
 
     it("Should throw error when price is negative", () => {
         expect(() => {
             let product = new Product("1", "43", -25);
-        }).toThrow("Name is required")
+        }).toThrowError("Name is required")
     });
+
+    it("Should change Name", () => {
+        const product = new Product("123", "Name", 100);
+        product.changeName("Product 2");
+        expect(product.name).toBe("Product 2");
+    });
+
+    it("Should change price", () => {
+        const product = new Product("123", "Product 1", 100);
+        product.changePrice(200);
+        expect(product.price).toBe(200);
+    })
 
 });
