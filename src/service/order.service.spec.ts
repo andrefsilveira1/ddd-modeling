35953387c094a947f9fed82ffe1b429ce37cc50c
André Freitas <freitasandre38@gmail.com>
2024-06-15 08:46:12 -0300

    feat: create order service test

---
+++ src/service/order.service.spec.ts
@@ -0,0 +1,14 @@
+import OrderItem from "../entity/items"
+import Order from "../entity/order";
+
+describe("Order service unit tests", () => {
+    it("Should get total of all orders", () => {
+        const item = new OrderItem("1", "item 1", 100);
+        const item2 = new OrderItem("2", "item 2", 100);
+        
+        const order = new Order("1", "1", [item], 200, "1", 2);
+        const order2 = new Order("2", "2", [item2], 200, "1", 2);
+        
+        const total = OrderService.total([order,order2]);
+    });
+})
