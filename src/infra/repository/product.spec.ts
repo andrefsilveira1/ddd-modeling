8553db769b2f66c44dcc091e9a8775b61922f3fd
André Freitas <freitasandre38@gmail.com>
2024-06-16 09:48:59 -0300

    feat: create product repository tests

--- src/infra/repository/product.spec.ts
+++ src/infra/repository/product.spec.ts
@@ -1,5 +1,7 @@
-import { Sequelize } from "sequelize"
+import { Sequelize } from "sequelize-typescript"
 import ProductModel from "../db/sequelize/model/product";
+import ProductService from "../../service/product.service";
+import Product from "../../domain/entity/product";
 
 describe("Product repository test", () => {
     let sequelize: Sequelize;
@@ -12,11 +14,26 @@ describe("Product repository test", () => {
             sync: { force: true },
         });
 
-        // sequelize.addModels({ProductModel}) Find why this does not work
+        sequelize.addModels([ProductModel]);
+        await sequelize.sync();
 
-        afterEach(async () => {
-            await sequelize.close();
-        }
-        )
     });
+
+    afterEach(async () => {
+        await sequelize.close();
+    }
+    );
+
+    it("Should create a product on repository", async () => {
+        const productRepository = new ProductRepository();
+        const product = new Product("1", "Product 1", 100);
+        await productRepository.create(product);
+
+        const productModel = await ProductModel.findOne({ where: { id: "1"}});
+        expect(productModel.toJSON()).toStrictEqual({
+            id: "1",
+            name: "Product 1",
+            price: 100,
+        });
+    })
 })
