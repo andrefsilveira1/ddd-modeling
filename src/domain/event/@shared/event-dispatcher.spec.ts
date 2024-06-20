ae3a96aad868fd17b326e0d7ece1a6e49f4c4a27
André Freitas <freitasandre38@gmail.com>
2024-06-20 08:32:08 -0300

    feat: implements event dispatcher test

---
+++ src/domain/event/@shared/event-dispatcher.spec.ts
@@ -0,0 +1,11 @@
+describe("Domain Events tests", () => {
+    it("Should register an event handler", () => {
+        const eventDispatcher = new EventDispatcher();
+        const eventHandler = new sendEmailWhenProductIsCreatedHandler();
+
+        eventDispatcher.register("ProductCreated", eventHandler);
+
+        expect(eventDispatcher.getEventHandlers["ProductCreated"]).toBeDefined();
+        expect(eventDispatcher.getEventHandlers["ProductCreated"].length).toBe(1);
+    })
+});
