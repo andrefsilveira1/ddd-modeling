95f7c5e339863e623cad48bbea6d1f573e1af0f7
André Freitas <freitasandre38@gmail.com>
2024-06-22 11:07:53 -0300

    feat: create register event method

--- src/domain/event/@shared/event-dispatcher.ts
+++ src/domain/event/@shared/event-dispatcher.ts
@@ -3,19 +3,30 @@ import EventHandlerInterface from "./event-handler.interface";
 import EventInterface from "./event.interface";
 
 export default class EventDispatcher implements EventDispatcherInterface {
+
+    private eventHandlers: { [eventName: string]: EventHandlerInterface[] } = {};
+
+    get getEventHandlers(): { [eventName: string]: EventHandlerInterface[] } {
+        return this.eventHandlers;
+    }
+
     notify(event: EventInterface): void {
-        
+
     }
 
     register(eventName: string, eventHandler: EventHandlerInterface<EventInterface>): void {
-        
+        if (!this.eventHandlers[eventName]) {
+            this.eventHandlers[eventName] = [];
+        }
+
+        this.eventHandlers[eventName].push(eventHandler)
     }
 
     unregister(eventName: string, eventHandler: EventHandlerInterface<EventInterface>): void {
-        
+
     }
 
     unregisterAll(): void {
-        
+
     }
 }
