2323282f829a279033313e5c678bdf8d53395cd1
André Freitas <freitasandre38@gmail.com>
2024-06-23 09:22:27 -0300

    feat: create notify events method

--- src/domain/event/@shared/event-dispatcher.spec.ts
+++ src/domain/event/@shared/event-dispatcher.spec.ts
@@ -1,4 +1,5 @@
 import sendEmailWhenProductIsCreatedHandler from "../product/handlers/send-email";
+import ProductCreatedEvent from "../product/product-created.event";
 import EventDispatcher from "./event-dispatcher";
 
 describe("Domain Events tests", () => {
@@ -6,36 +7,52 @@ describe("Domain Events tests", () => {
         const eventDispatcher = new EventDispatcher();
         const eventHandler = new sendEmailWhenProductIsCreatedHandler();
 
-        eventDispatcher.register("ProductCreated", eventHandler);
+        eventDispatcher.register("ProductCreatedEvent", eventHandler);
 
-        expect(eventDispatcher.getEventHandlers["ProductCreated"]).toBeDefined();
-        expect(eventDispatcher.getEventHandlers["ProductCreated"].length).toBe(1);
-        expect(eventDispatcher.getEventHandlers["ProductCreated"][0]).toMatchObject(eventHandler)
+        expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"]).toBeDefined();
+        expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"].length).toBe(1);
+        expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"][0]).toMatchObject(eventHandler)
     });
 
     it("Should unregister event handler", () => {
         const eventDispatcher = new EventDispatcher();
         const eventHandler = new sendEmailWhenProductIsCreatedHandler();
 
-        eventDispatcher.register("ProductCreated", eventHandler);
+        eventDispatcher.register("ProductCreatedEvent", eventHandler);
 
-        eventDispatcher.unregister("ProductCreated", eventHandler);
+        eventDispatcher.unregister("ProductCreatedEvent", eventHandler);
 
-        expect(eventDispatcher.getEventHandlers["ProductCreated"]).toBeDefined();
-        expect(eventDispatcher.getEventHandlers["ProductCreated"].length).toBe(0);
+        expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"]).toBeDefined();
+        expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"].length).toBe(0);
     });
 
     it("Should unregister event handler", () => {
         const eventDispatcher = new EventDispatcher();
         const eventHandler = new sendEmailWhenProductIsCreatedHandler();
 
-        eventDispatcher.register("ProductCreated", eventHandler);
+        eventDispatcher.register("ProductCreatedEvent", eventHandler);
 
         eventDispatcher.unregisterAll();
 
-        expect(eventDispatcher.getEventHandlers["ProductCreated"]).toBeUndefined();
+        expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"]).toBeUndefined();
     });
 
 
+    it("Should notify event handlers", () => {
+        const eventDispatcher = new EventDispatcher();
+        const eventHandler = new sendEmailWhenProductIsCreatedHandler();
+        const spyEvent = jest.spyOn(eventHandler, "handle");
 
+        eventDispatcher.register("ProductCreatedEvent", eventHandler);
+
+        const productCreatedEvent = new ProductCreatedEvent({
+            name: "Product 1",
+            description: "Description from product 1",
+            price: 10.0,
+        });
+
+        eventDispatcher.notify(productCreatedEvent);
+        
+        expect(spyEvent).toHaveBeenCalled();
+    });
 });
